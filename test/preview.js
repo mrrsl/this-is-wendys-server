8f8ce3fecd91e9c768b9e12a50db9919e9d2ba0f
Morris L <morrislli@outlook>
2025-11-08 12:12:10 -0800

    more reliable removal from group table

---
+++ test/preview.js
@@ -0,0 +1,63 @@
+const socketEndpoint = "wss://localhost:3000/ws?group=";
+//const socketEndpoint = "wss://this-is-wendys-socket-service-dkc8eyd7bzc9hndh.canadacentral-01.azurewebsites.net/ws?group=";
+const pairCode = "mygoat";
+/**
+ * Sets up the socket connection to the backend service
+ * @param {String} pairingCode 
+ * @param {(MessageEvent) => void} onmessage
+ * @param {(Event) => void} onopen
+ * @param {(CloseEvent) => void} onclose
+ */
+function createConnection(pairingCode, onmessage, onclose, onopen) {
+    let socket = new WebSocket(socketEndpoint + pairCode);
+    onmessage && socket.addEventListener("message", onmessage);
+    onclose && socket.addEventListener("close", onclose);
+    onopen && socket.addEventListener("open", onopen);
+    return socket;
+}
+
+/**
+ * Default message handler for printing to console.
+ * @param {MessageEvent} mevent 
+ */
+function messageHandler(mevent) {
+    console.log(`Data: ${mevent.data}`);
+}
+
+function init(event) {
+    const output = document.querySelector("#display");
+    const input = document.querySelector("input");
+    const enter = document.querySelector("button");
+
+    output.consoleQueue = [];
+
+    function sendLine(text) {
+        let line = document.createElement("p");
+        line.textContent = text;
+        output.consoleQueue.push(line);
+        output.append(line);
+        if (output.consoleQueue.length > 20) {
+            output.consoleQueue.shift().remove();
+        }
+    }
+
+    output.attachedWs = createConnection(pairCode,
+        (msg) => {
+            sendLine(msg.data);
+        },
+        (close) => {
+            sendLine("Connection closed");
+        },
+        (open) => {
+            sendLine("Connection opened");
+        }
+    );
+
+    enter.addEventListener("click", (event) => {
+        debugger;
+        output.attachedWs.send(input.value);
+        sendLine(`Message sent ${input.value}`);
+        input.value = "";
+    });
+}
+init();
