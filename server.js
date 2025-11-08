67ee2ddac8268014797b7ef21485a09acc824c22
Morris L <morrislli@outlook>
2025-11-08 08:45:32 -0800

    added STUN endpoint

--- server.js
+++ server.js
@@ -9,5 +9,11 @@ const server = express();
 server.get("/", (req, res) => {
     res.sendFile(path.resolve("./test/index.html"));
 });
+/**
+ * Return client's IP to itself.
+ */
+server.get("/stun", (req, res) => {
+    res.send(req.ip);
+});
 
 const manager = new SocketManager(server.listen(process.env.PORT || 3000));
